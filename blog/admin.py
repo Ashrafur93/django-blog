b957fa4337c0654623ab3a3c97160bb05297abf6
Ashrafur93 <ashrafur_ahmed@hotmail.co.uk>
2025-01-26 03:13:20 +0000

    Add collaboration request form and update About page layout

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,10 @@
+
 from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
-@admin.register(Post)
 
+@admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'slug', 'status', 'created_on')
@@ -11,6 +12,7 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status', 'created_on',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
+
 
 # Register your models here.
 admin.site.register(Comment)
